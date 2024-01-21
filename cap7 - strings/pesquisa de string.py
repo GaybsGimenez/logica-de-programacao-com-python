fd4c239ab71607cfcb79dd32645d2192219a384c
Gabriela Gimenez <81652916+GaybsGimenez@users.noreply.github.com>
2024-01-21 18:29:03 -0300

    update

--- cap7 - strings/pesquisa de string.py
+++ cap7 - strings/pesquisa de string.py
@@ -1,4 +1,20 @@
 """
-
+Para pesquisar se uma string está dentro de outra e obter o índice da primeira ocorrência, use o método find().
 
 """
+
+s = "Alô mundo"
+s.find("mun")
+s.find("ok")
+
+# O método find() retorna -1 se a string não for encontrada. e o índice da primeira ocorrência se for encontrada.
+
+"""
+Se o objetivo for pesquisar a última ocorrência de uma string, use o método rfind().
+
+"""
+s = "Um dia de sol"
+s.rfind("d")
+# 7, pois a última ocorrência de d está no índice 7 da string s, contando a partir do 0
+
+ 
